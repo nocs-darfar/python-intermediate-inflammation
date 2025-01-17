d496e5577b8f08951cf76047db7f49d6161bde86
D. Farrell <dara.farrell@noc.ac.uk>
2025-01-17 09:51:38 +0000

    updates to inflamm and test scripts

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pytest
 import numpy.testing as npt
+import pytest_cov
 
 from inflammation.models import daily_mean
 
@@ -64,3 +65,25 @@ from inflammation.models import daily_mean
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+    def patient_normalise(data):
+        """
+        Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+        Any NaN values are ignored, and normalised to 0
+
+        :param data: 2D array of inflammation data
+        :type data: ndarray
+
+        """
+        if not isinstance(data, np.ndarray):
+            raise TypeError('data input should be ndarray')
+        if len(data.shape) != 2:
+            raise ValueError('inflammation array should be 2-dimensional')
+        if np.any(data < 0):
+            raise ValueError('inflammation values should be non-negative')
+        max = np.nanmax(data, axis=1)
+        with np.errstate(invalid='ignore', divide='ignore'):
+            normalised = data / max[:, np.newaxis]
+        normalised[np.isnan(normalised)] = 0
+        return normalised
